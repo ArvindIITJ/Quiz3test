1b079998b9ba7e8f90ae4a412540bc80bee09eae
ArvindIITJ <sriram.2@iitj.ac.in>
2022-11-20 22:12:55 +0530

    exam

--- plots.py
+++ plots.py
@@ -1,45 +1,103 @@
-# Standard scientific Python imports
-import matplotlib.pyplot as plt
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+
+# PART: library dependencies -- sklear, torch, tensorflow, numpy, transformers
 
 # Import datasets, classifiers and performance metrics
-from sklearn import datasets, svm, metrics
-from sklearn.model_selection import train_test_split
+from sklearn import datasets, svm, metrics, tree
+import pdb
+import argparse
+
+from utils import (
+    preprocess_digits,
+    train_dev_test_split,
+    data_viz,
+    get_all_h_param_comb,
+    tune_and_save,
+    macro_f1
+)
+from joblib import dump, load
+
+parser = argparse.ArgumentParser(description = 'Input for classifier and random state')
+parser.add_argument('clf_name',type=str)
+parser.add_argument('random_state',type=int)
+args = parser.parse_args()
+
+train_frac, dev_frac, test_frac = 0.8, 0.1, 0.1
+assert train_frac + dev_frac + test_frac == 1.0
+
+# 1. set the ranges of hyper parameters
+gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
+c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
+
+svm_params = {}
+svm_params["gamma"] = gamma_list
+svm_params["C"] = c_list
+svm_h_param_comb = get_all_h_param_comb(svm_params)
+
+max_depth_list = [2, 10, 20, 50, 100]
+
+dec_params = {}
+dec_params["max_depth"] = max_depth_list
+dec_h_param_comb = get_all_h_param_comb(dec_params)
+
+h_param_comb = {"svm": svm_h_param_comb, "decision_tree": dec_h_param_comb}
+
+# PART: load dataset -- data from csv, tsv, jsonl, pickle
 digits = datasets.load_digits()
+data_viz(digits)
+data, label = preprocess_digits(digits)
+# housekeeping
+del digits
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
-# flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+# define the evaluation metric
+metric_list = [metrics.accuracy_score, macro_f1]
+h_metric = metrics.accuracy_score
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+n_cv = 1
+results = {}
+for n in range(n_cv):
+    x_train, y_train, x_dev, y_dev, x_test, y_test = train_dev_test_split(
+        data, label, train_frac, dev_frac, args.random_state
+    )
+    # PART: Define the model
+    # Create a classifier: a support vector classifier
+    models_of_choice = {
+        "svm": svm.SVC(),
+        "decision_tree": tree.DecisionTreeClassifier(),
+    }
+    # for clf_name in models_of_choice:
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.4, shuffle=False
-)
+    clf = models_of_choice[args.clf_name]
+    print("[{}] Running hyper param tuning for {}".format(n,args.clf_name))
+    actual_model_path = tune_and_save(
+        clf, x_train, y_train, x_dev, y_dev, h_metric, h_param_comb[args.clf_name], model_path=None
+    )
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+    # 2. load the best_model
+    best_model = load(r"../models/"+actual_model_path)
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
+    # PART: Get test set predictions
+    # Predict the value of the digit on the test subset
+    predicted = best_model.predict(x_test)
+    if not args.clf_name in results:
+        results[args.clf_name]=[]    
 
-plt.show()
+    results[args.clf_name].append({m.__name__:m(y_pred=predicted, y_true=y_test) for m in metric_list})
+
+    f = open(r"../results/"+actual_model_path+".txt","w+")
+    for s in results:
+        f.write("test accuracy: "+str(results[s][0]['accuracy_score'])+"\n")
+        f.write("test macro-f1: "+str(results[s][0]['macro_f1'])+"\n")
+        f.write("Model saved at: "+"./models/"+actual_model_path+"\n")
+    
+
+    # 4. report the test set accurancy with that best model.
+    # PART: Compute evaluation metrics
+    print(
+        f"Classification report for classifier {clf}:\n"
+        f"{metrics.classification_report(y_test, predicted)}\n"
+    )
+    f.close()
+print(results)
